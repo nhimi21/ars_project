95cc51009566e185c81dce186efc01d8bb53a2cb
Hekuran Himi <hekuranhimi@gmail.com>
2022-01-18 18:56:24 +0100

    cm3

--- src/main/java/com/himi/ars_project/models/Invoice.java
+++ src/main/java/com/himi/ars_project/models/Invoice.java
@@ -39,7 +39,9 @@ public class Invoice {
     private List<User> userIn;
     public Invoice() {
     }
-//sfadgfadgaef
+//sfadgfadgaef   dwfsdfsdfa
+
+    //sadasd
     public Invoice(Integer invoice_num, String flight_name, String full_name, LocalDate dep_date, LocalDate arri_date, String num_of_person, String pnr_code, Float total_price, Date createdAt, Date updatedAt, Flight flight, List<User> userIn) {
         this.invoice_num = invoice_num;
         this.flight_name = flight_name;
